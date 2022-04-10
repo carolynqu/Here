32f7f8859ba3dcab50ccbb935dcdf76d66416db4
carolynqu <67798872+carolynqu@users.noreply.github.com>
2022-04-10 06:37:01 -0400

    main page

--- frontend/src/screens/Home/Home.jsx
+++ frontend/src/screens/Home/Home.jsx
@@ -1,23 +1,23 @@
-import { StyleSheet, Text, View } from 'react-native';
-import theme from '../../theme.style';
-import Sessions from './Sessions';
+import { StyleSheet, Text, View } from "react-native";
+import Sessions from "./Sessions";
+
+import theme from "../../theme.style";
+import { globalStyles } from "../../GlobalStyles";
 
 const Home = () => {
-  	return (
-  	  	<View>
-  	  		<Text style={styles.title}>
-  	  			Current Events
-  	  		</Text>
-  	  		<Sessions/>
-  	  	</View>
-  	);
-}
+  return (
+    <View style={{ marginHorizontal: theme.horizMargin }}>
+      <Text style={globalStyles.mainPageHeader}>Current Events</Text>
+      <Sessions />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-	title: {
-		fontSize: 20,
-		margin: 10,
-		color: theme.darkText,
-	},
+  title: {
+    fontSize: 20,
+    margin: 10,
+    color: theme.darkText,
+  },
 });
 export default Home;
